02ace2ebfe1e916202d344dc233f171109600136
vodvarka <vodvarka@sefira.cz>
2020-09-28 16:26:10 +0200

    Refactored project

---
+++ src/main/java/lu/nowina/nexu/api/CertificateFilterHelper.java
@@ -0,0 +1,30 @@
+package lu.nowina.nexu.api;
+
+import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
+import eu.europa.esig.dss.token.SignatureTokenConnection;
+import eu.europa.esig.dss.tsl.KeyUsageBit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Provides filtering capabilities for product adapters.
+ *
+ * @author Landry Soules
+ *
+ */
+public class CertificateFilterHelper {
+
+	public List<DSSPrivateKeyEntry> filterKeys(SignatureTokenConnection token, CertificateFilter filter) {
+		if (filter.getNonRepudiationBit()) {
+			List<DSSPrivateKeyEntry> filteredList = new ArrayList<>();
+			for (DSSPrivateKeyEntry entry : token.getKeys()) {
+				if (entry.getCertificate().checkKeyUsage(KeyUsageBit.nonRepudiation)) {
+					filteredList.add(entry);
+				}
+			}
+			return filteredList;
+		}
+		return token.getKeys();
+	}
+}
