c12b9e20187e97b761921a0701a1c2fba3d3c573
hlavnicka-sefira <66832977+hlavnicka-sefira@users.noreply.github.com>
2021-03-05 12:01:11 +0100

    OBPK-61 - add copyright license notice

--- src/main/java/lu/nowina/nexu/json/DigestAlgorithmAdapter.java
+++ src/main/java/lu/nowina/nexu/json/DigestAlgorithmAdapter.java
@@ -1,3 +1,16 @@
+/**
+ * © SEFIRA spol. s r.o., 2020-2021
+ *
+ * Licensed under EUPL Version 1.1 or - upon approval by the European Commission - later versions of the EUPL (the "License").
+ * You may use this work only in accordance with the License.
+ * You can obtain a copy of the License at the following address:
+ *
+ * http://ec.europa.eu/idabc/eupl5
+ *
+ * Unless there is a legal or contractual obligation in writing, the software distributed under the License is distributed "as is",
+ * WITHOUT WARRANTIES OR CONDITIONS WHATSOEVER, express or implied.
+ * See the License for specific permissions and language restrictions under the License.
+ */
 package lu.nowina.nexu.json;
 
 /*
