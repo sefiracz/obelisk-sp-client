0bc980964ba26fd7bc35e08537922b20b8a304e6
hlavnicka-sefira <66832977+hlavnicka-sefira@users.noreply.github.com>
2023-10-12 16:28:36 +0200

    - v2.0.8 - fix untrusted SSL dialog not appearing - silence excessive debug log from Microstream lib

--- sp-client/src/main/java/cz/sefira/obelisk/api/ws/auth/AuthenticationProvider.java
+++ sp-client/src/main/java/cz/sefira/obelisk/api/ws/auth/AuthenticationProvider.java
@@ -10,12 +10,24 @@ package cz.sefira.obelisk.api.ws.auth;
  * Author: hlavnicka
  */
 
+import cz.sefira.obelisk.api.ws.ssl.SSLCommunicationException;
+
 /**
- * description
+ * Authentication provider interface
  */
 public interface AuthenticationProvider {
 
+  /**
+   * Returns URI where to redirect HTTP client to use this auth credentials (might be null)
+   * @return Endpoint URI where to use given authentication credentials (might be null if not applicable)
+   */
   String getRedirectUri();
 
-  String getEndpointAuthentication() throws AuthenticationProviderException;
+  /**
+   * Returns endpoint authentication credentials
+   * @return Authentication credentials
+   * @throws AuthenticationProviderException
+   * @throws SSLCommunicationException
+   */
+  String getEndpointAuthentication() throws AuthenticationProviderException, SSLCommunicationException;
 }
